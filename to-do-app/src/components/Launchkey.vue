81e3e86df037fe9285a30a76717073dd38e93ad9
bgroveben <bgroveben@gmail.com>
2018-12-01 02:55:13 -0800

    Add new Launchkey component to the Vue app

---
+++ to-do-app/src/components/Launchkey.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>I would love to work for LaunchKey, but I'm not sure if I will be useful to you. LaunchKey won the first hackathon I ever went to (Startup Weekend 2012, I tweeted about it, first tweet too) and I have been keeping an eye on their work ever since. One of the only reasons I understand what multifactor authentication is is because I've been watching the launchkey-python repo on Github.</p>
+    </div>
+</template>
+
+<script type="text/javascript">
+export default {
+};
+</script>
+<style></style>
