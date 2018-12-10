e368c683b9e2b2e1189b68d7636d33170589f6d2
bgroveben <bgroveben@gmail.com>
2018-12-10 02:46:27 -0800

    Fix linter issues and start rendering data

--- to-do-app/src/components/TodoList.vue
+++ to-do-app/src/components/TodoList.vue
@@ -1,44 +1,7 @@
 <template>
-    <div>
-        <ul>
-        <li> Task A </li>
-        <li> Task B</li>
-        <li> Task C </li>
-        </ul>
-    </div>
+  <div>
+    // Javascript expressions in Vue are enclosed in double curly brackets
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done == true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done == false}).length}}</p>
+  </div>
 </template>
-
-<script type = "text/javascript" >
-
-export default {
-name: 'app',
-components: {
-TodoList,
-},
-props: ['todos'],
-// Make data available to the template
-data() {
-return {
-todos: [{
-    title: 'Todo A',
-    project: 'Project A',
-    done: false,
-}, {
-    title: 'Todo B',
-    project: 'Project B',
-    done: true,
-}, {
-    title: 'Todo C',
-    project: 'Project C',
-    done: false,
-}, {
-    title: 'Todo D',
-    project: 'Project D',
-    done: false,
-}],
-};
-},
-};
-</script>
-<style>
-</style>
